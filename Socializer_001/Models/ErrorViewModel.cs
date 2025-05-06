daa1b32e4c5a5046ed1ee4380f91842f1bfb94ef
Radmehr Vosoughi <radmehrvosooghi@gmail.com>
2025-05-06 22:52:10 +0330

    Add project files.

---
+++ Socializer_001/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Socializer_001.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
