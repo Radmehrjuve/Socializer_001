f8330cc618f9020f85c6be893ab940194e4b610f
Radmehr Vosoughi <radmehrvosooghi@gmail.com>
2025-05-08 01:26:05 +0330

    1

---
+++ OpenAi_APi/Controllers/OpenAiController.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace OpenAi_APi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAiController : ControllerBase
+    {
+        private readonly IChatCompletionService _chatCompletionService;
+        public OpenAiController(IChatCompletionService cmp)
+        {
+                _chatCompletionService = cmp;
+        }
+        public async Task<string> GetAwsner(List<Message> messages)
+        {
+
+        }
+    }
+}
--- OpenAi_APi/Program.cs
+++ OpenAi_APi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddKernel();
+var kernelbuilder = Kernel.CreateBuilder();
+builder.Services.AddOpenAIChatCompletion(modelId: "gpt-4o-mini", apiKey: builder.Configuration["Ai:OpenAi:ApiKey"]);
 
+var kernel = kernelbuilder.Build();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
