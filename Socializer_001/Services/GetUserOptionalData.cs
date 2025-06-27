429b367f9b0947249d148d6e5a1cc14dc5b0ef45
Radmehr Vosoughi <radmehrvosooghi@gmail.com>
2025-06-27 02:44:04 +0200

    feat: firstname in layout home page fix: personaldata page buttons fix

--- Socializer_001/Services/GetUserOptionalData.cs
+++ Socializer_001/Services/GetUserOptionalData.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Socializer_001.Areas.Identity.Data;
 using Socializer_001.Models;
 
@@ -17,7 +18,29 @@ namespace Socializer_001.Services
         public async Task<ApplicationUser> GetUserFullDataAsync(IdentityUser user)
         {
             FullUser = AuthenticationDBContext.Users.FirstOrDefault(x => x.Id == user.Id);
-            return FullUser;
+            if (FullUser != null)
+            {
+                return FullUser;
+            }
+            else throw new ArgumentNullException(nameof(user), "User Not Found!");
+        }
+        public async Task<ApplicationUser> GetUserByIDAsync(string userId)
+        {
+            FullUser = await AuthenticationDBContext.Users.FirstOrDefaultAsync(x => x.Id == userId);
+            if (FullUser != null)
+            {
+                return FullUser;
+            }
+            else throw new ArgumentNullException(nameof(userId), "User Not Found!");
+        }
+        public async Task<string> GetUserFirstName(string userId)
+        {
+            FullUser = await GetUserByIDAsync(userId);
+            if (FullUser != null)
+            {
+                return FullUser.FirstName;
+            }
+            else throw new ArgumentNullException(nameof(userId),"User Not Found!");
         }
     }
 }
